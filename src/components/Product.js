5e34c7d39e9bcb67be203d5595a828405659a1a6
Nicole Tilbe <nicolecali2406@outlook.com>
2020-09-08 19:43:11 -0400

    MVP and stretch done

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { ProductContext } from '../contexts/ProductContext'
+
 
 const Product = props => {
+	
+
 	return (
 		<div className="product">
 			<img src={props.product.image} alt={`${props.product.title} book`} />
